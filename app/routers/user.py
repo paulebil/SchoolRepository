783d242be7d89aa2e0991cc8602d9c85e9d5164a
paulebil <ebilpaul06@gmail.com>
2025-09-05 16:40:05 +0300

    added email templates and functionality

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, BackgroundTasks
 
 from app.database.database import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -17,5 +17,5 @@ def get_user_service(session: AsyncSession = Depends(get_session)) -> UserServic
     return UserService(user_repository)
 
 @admin_user_router.post("/user", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
-async def create_admin_user(data: CreateAdminUser, user_service: UserService = Depends(get_user_service)):
-    return await user_service.create_admin_user(data)
+async def create_admin_user(data: CreateAdminUser, background_tasks: BackgroundTasks, user_service: UserService = Depends(get_user_service)):
+    return await user_service.create_admin_user(data, background_tasks)
--- app/service/user.py
+++ app/service/user.py
@@ -1,8 +1,9 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, BackgroundTasks
 
 from app.repository.user import UserRepository
 from app.schemas.user import *
 from app.models.user import User, UserRole
+from app.service.email_service import UserAuthEmailService
 
 from app.core.security import Security
 
@@ -12,7 +13,7 @@ class UserService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
 
-    async def create_admin_user(self, data: CreateAdminUser) -> UserResponse:
+    async def create_admin_user(self, data: CreateAdminUser, background_tasks: BackgroundTasks) -> UserResponse:
         user_exists = await self.user_repository.get_user_by_email(str(data.email))
         if user_exists:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User with this email already exists.")
@@ -22,4 +23,5 @@ class UserService:
         user_to_create = User(**user_dict)
         user_to_create.role = UserRole.ADMIN
         user = await self.user_repository.create_user(user_to_create)
+        await UserAuthEmailService.send_account_verification_email(user, background_tasks)
         return UserResponse.model_validate(user)
