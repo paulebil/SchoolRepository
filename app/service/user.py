6f85b82e9328178e4f674c3ec4fda4f4ad8a9566
paulebil <ebilpaul06@gmail.com>
2025-09-05 14:12:33 +0300

    created user service layer

---
+++ app/service/user.py
@@ -0,0 +1,8 @@
+from app.repository.user import UserRepository
+
+class UserService:
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
+
+    async def create_user(self, data):
+        pass
